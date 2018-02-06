96854ce17aeefb7c9b4633946e21950d3f8ed526
Fuatari <sam.western@gmail.com>
2018-02-06 16:35:13 +0000

    First commit

---
+++ src/TicketCalcTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class TicketCalcTest {
+
+	TicketCalculator ticketCalc = new TicketCalculator();
+
+	@Test
+	void nonWednesdayPrices() {
+		fail("TBA");
+	}
+
+	void wednesdayPrices() {
+		fail("TBA");
+	}
+
+}
---
+++ src/TicketPricesTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class TicketPricesTest {
+
+	TicketPrices price = new TicketPrices();
+
+	@Test
+	void confirmStandardPrice() {
+		fail("TBA");
+	}
+
+	void confirmOAPPrice() {
+		fail("TBA");
+	}
+
+	void confirmStudentPrice() {
+		fail("TBA");
+	}
+
+	void confirmChildPrice() {
+		fail("TBA");
+	}
+}
