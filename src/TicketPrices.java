d3e633197b61d2c2ae56bdb871400e7402fcfb28
Fuatari <sam.western@gmail.com>
2018-02-06 17:31:58 +0000

    Created map of customers and prices with tests

--- src/TicketPrices.java
+++ src/TicketPrices.java
@@ -1,4 +1,33 @@
+import java.util.HashMap;
 
 public class TicketPrices {
+
+	HashMap<Integer, Customer> ticketMap = new HashMap<Integer, Customer>();
+
+	public Customer createCustomer(String type, int price) {
+		Customer customer = new Customer(type, price);
+		return customer;
+	}
+
+	public void setTicketPrices() {
+		addCustomerType(1, createCustomer("Standard", 8));
+		addCustomerType(2, createCustomer("OAP", 6));
+		addCustomerType(3, createCustomer("Student", 6));
+		addCustomerType(4, createCustomer("Child", 4));
+	}
+
+	public void addCustomerType(int id, Customer customer) {
+		ticketMap.put(id, customer);
+	}
+
+	public int getCustomerPrice(String type) {
+		int price = 0;
+		for (int i = 1; i <= ticketMap.size(); i++) {
+			if (ticketMap.get(i).getType().equalsIgnoreCase(type)) {
+				price = ticketMap.get(i).getPrice();
+			}
+		}
+		return price;
+	}
 
 }
